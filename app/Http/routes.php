b8dafd0be4d2d4b341711897c23ebc7b0a9e2b89
gregpiatperso <gregoire.piat@outlook.fr>
2016-02-08 19:29:02 +0100

    Ajout du groupe de routes d'aministration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+//Routes pour toute la partie administration
+Route::group(['prefix' => 'admin'], function(){
+    //TODO Ajouter ici les routes pour l'administration
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
